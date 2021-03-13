da2eca6614a6b685cd216095d8cea6c3697ae8c0
chaouiki1 <chadjahmed@southbronxprep.net>
2021-03-13 20:18:00 +0000

    suirrel chaouiki

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import './App.css';
+import Squirrel from './Components/Squirrel';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state={
+        squirrels:[],
+    }
   }
   
-  // add a componentDidMount lifecycle method to fetch data from the API
-
+    //add a componentDidMount lifecycle method to fetch data from the API
+    componentDidMount = () => {
+        let appToken = 'uumi4lxjUXJp4qHkHomGe34CC'
+        let query = `https://data.cityofnewyork.us/resource/vfnx-vebw.json?$$app_token=${appToken}&running=true`
+        fetch(query)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            this.setState(state => {
+                state.squirrels = data 
+                return state
+            })
+        })
+        .catch(e => console.log(e))
+    }
   render() {
     return (
       <div className="App">
         <h1>2018 NYC Squirrel Survey Data</h1>
+        <h3>{this.state.squirrels.length}</h3>
+        {this.state.squirrels.map(squirrel => <Squirrel data={squirrel}/>)}
       </div>
     );
   }
--- src/Components/Squirrel.js
+++ src/Components/Squirrel.js
@@ -4,6 +4,9 @@ function Squirrel(props) {
   return (
     <div className="Squirrel">
       <h3>SQUIRREL CARD!</h3>
+      <h5> age:{props.data.age}</h5>
+      <h5> locaation:{props.data.location}</h5>
+      <h5> shift:{props.data.shift}</h5>
     </div>
   );
 }
